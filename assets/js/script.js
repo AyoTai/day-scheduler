25b1750fdc43453273d1e92a52082d51d550163d
Tai Mujarin <tai.mujarin@gmail.com>
2021-05-25 21:38:51 -0700

    added storage and input

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,41 @@ var time = [
     "time0300",
     "time0400",
     "time0500"
-]
-
+  ]
+  
+// Selectors //
+var saveBtn = $("#saveBtn");
+var tableRow = $("tr");
+  
+  
+  
 currentTime = setInterval(function () {
-    $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss A'));
+    $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss A, dddd'));
 }, 1000);
+  
+console.log($('#currentDay'));
 
-console.log(currentTime);
+// Creating text input //
+tableRow.on("click", ".content", function (){
+    var inputText = $("<input type ='text'>");
+    $(this).children().replaceWith(inputText);
+    inputText.trigger("focus");
+})
 
+// Save Button 
+tableRow.on("click", saveBtn, function (){
+    $("body").find("input").val();
+    console.log($("body").find("input").val())
+})
+
+console.log($("body").find("input").val());
+
+
+// Local Storage
+function setLS() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function getLocalStorage() {
+    time = JSON.parse(localStorage.getItem("tasks"));
+}
