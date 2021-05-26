50b763e0636cc662f6d3f80ae7ff8f6b458740e5
Tai Mujarin <tai.mujarin@gmail.com>
2021-05-25 22:32:16 -0700

    made input focus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
 // Variasbles //
-var time = [
-    "time0900",
-    "time1000",
-    "time1100",
-    "time1200",
-    "time0100",
-    "time0200",
-    "time0300",
-    "time0400",
-    "time0500"
-  ]
-  
+var timeDuty = [
+    { time: "time0900", input: "" },
+    { time: "time1000", input: "" },
+    { time: "time1100", input: "" },
+    { time: "time1200", input: "" },
+    { time: "time0100", input: "" },
+    { time: "time0200", input: "" },
+    { time: "time0300", input: "" },
+    { time: "time0400", input: "" },
+    { time: "time0500", input: "" },
+]
+
 // Selectors //
 var saveBtn = $("#saveBtn");
 var tableRow = $("tr");
   
   
   
-currentTime = setInterval(function () {
+currentDay = setInterval(function () {
     $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss A, dddd'));
 }, 1000);
   
@@ -25,9 +25,15 @@ console.log($('#currentDay'));
 
 // Creating text input //
 tableRow.on("click", ".content", function (){
-    var inputText = $("<input type ='text'>");
+    var inputText = $("<input type ='text'>"); //
     $(this).children().replaceWith(inputText);
     inputText.trigger("focus");
+})
+
+tableRow.on("blur", "input", function (){
+    inputTextValue = $(this).val();
+    var empty = $("<p class='p-content'>"); //
+    $(this).replaceWith(empty);
 })
 
 // Save Button 
@@ -38,12 +44,23 @@ tableRow.on("click", saveBtn, function (){
 
 console.log($("body").find("input").val());
 
+function findArray(time) {
+    if(timeDuty === undefined){
+        return;
+    } else {
+        for (var i = 0; i < timeDuty.length; i++){
+            if (timeDuty[i]["time"] === time){
+                timeDuty[i]["input"] = inputTextValue;
+            }
+        }
+    }
+}
 
 // Local Storage
 function setLS() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("timeDuty", JSON.stringify(tasks));
 }
 
 function getLocalStorage() {
-    time = JSON.parse(localStorage.getItem("tasks"));
+    time = JSON.parse(localStorage.getItem("timeDuty"));
 }
